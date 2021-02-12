a96d29d4545d6c9a18e9116fb4b69b2ba1169e75
murshidazher <murshidazher@yahoo.com>
2021-02-12 23:15:14 +0530

    :sparkles: feat: add sketch@61 cask

---
+++ Casks/sketch@61.1.rb
@@ -0,0 +1,24 @@
+cask 'sketch' do
+  version '61-89581'
+  sha256 '70329859429809474edc798a3efe24677efa9e4d1983ffc185bbea5570877815'
+  version '61.1-89650'
+  sha256 'ed19f92da751b11899845bfd26bb447dcc26582508eb23a9b4397ee5af5898a9'
+
+  url "https://download.sketchapp.com/sketch-#{version}.zip"
+  appcast 'https://download.sketchapp.com/sketch-versions.xml'
+  name 'Sketch'
+  homepage 'https://www.sketchapp.com/'
+  auto_updates true
+  depends_on macos: '>= :high_sierra'
+  app 'Sketch.app'
+  zap trash: [
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.bohemiancoding.sketch3.sfl*',
+               '~/Library/Application Support/com.bohemiancoding.sketch3',
+               '~/Library/Caches/com.bohemiancoding.sketch3',
+               '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.bohemiancoding.sketch3',
+               '~/Library/Logs/com.bohemiancoding.sketch3',
+               '~/Library/Preferences/com.bohemiancoding.sketch3.LSSharedFileList.plist',
+               '~/Library/Preferences/com.bohemiancoding.sketch3.plist',
+               '~/Library/Cookies/com.bohemiancoding.sketch3.binarycookies',
+             ]
+end
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# homebrew-sketch-63.1
-A homebrew tap for sketch 63.1 before license update
+# homebrew-murshid
+
+> A repository to maintain the homebrew taps and casks which are outdated or mine.
